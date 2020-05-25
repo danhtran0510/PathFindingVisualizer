5c6b2825e96eb39949bca548ed4ecdfb75ec1824
Tran Dung Danh <trandungdanh@mecury.local>
2020-05-25 14:10:52 +0700

    init

---
+++ src/app/components/PathFindingVisualizer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom'
+import './PathFindingVisualizer.css';
+
+export default class PathFindingVisualizer extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <h1>Hello World!</h1>
+        )
+    }
+}
